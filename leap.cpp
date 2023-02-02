c25d94f0d4887ca89fdfdf4570fce8712f1ea9c2
Daphne Luzong <daphneluzong@daphnes-air.lan>
2023-02-01 19:41:18 -0500

    edited code and added comments

--- leap.cpp
+++ leap.cpp
@@ -1,16 +1,24 @@
+/* 
+Daphne Luzong
+username: dluzong
+Task C: A leap year or a common year?
+    - Asks the user to input an integer representing a year number (1999, 2016, etc.) If the year is
+    a leap year, the program will print "leap year" and "common year" otherwise.
+*/
+
 #include <iostream>
 
 int main(){
    int i;
-   std::cout << "Enter year:";
+   std::cout << "Enter year: ";
    std::cin >> i;
-   if (i % 4){
+   if (i % 4 != 0){
     std::cout << "Common year\n";
    }
-   else if (i % 100){
+   else if (i % 100 != 0){
     std::cout << "Leap year\n";
    }
-   else if (i % 400){
+   else if (i % 400 != 0){
     std::cout <<"Common year\n";
    }
    else{
