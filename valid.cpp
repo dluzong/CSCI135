0bc736e76a20b3fd13fde1e9e17aafa29f7ee47d
Daphne Luzong <daphneluzong@daphnes-air.lan>
2023-02-07 21:17:07 -0500

    made slight edit to code

--- valid.cpp
+++ valid.cpp
@@ -6,7 +6,7 @@ Lab2A
 
 User is asked to input an integer in the range 0 < n < 100
 If number is out of range, program asks user to reinput a valid number
-valid number is then printed n squared.
+Once a valid number is obtained, then it is printed n squared.
 */
 
 #include <iostream>
@@ -15,12 +15,10 @@ int main(){
     int n;
     std::cout << "Please enter an integer: ";
     std::cin >> n;
-    while (n < 0 || n > 100){
+    while (n <= 0 || n >= 100){
         std::cout << "Please re-enter: ";
         std::cin >> n;
     }
-    if (0 < n && n < 100){
-        std::cout << "Number squared is " << n * n << std::endl;
-    }
+    std::cout << "Number squared is " << n * n << std::endl;
     return 0;
 }
