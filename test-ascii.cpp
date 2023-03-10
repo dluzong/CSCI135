54ff90c94ec65919ef4a1e76a10a58666a3dab17
Daphne Luzong <daphneluzong@daphnes-air.lan>
2023-03-10 01:29:09 -0500

    task a complete w comments

--- test-ascii.cpp
+++ test-ascii.cpp
@@ -9,13 +9,19 @@ along with their ASCII codes
 #include <cctype>
 
 int main(){
-    std::string text;
-    text = "Cat :3 Dog";
+    std::string text = "Cat :3 Dog";
+    //std::string text2 = "AbcD";
 
     std::cout << "Input: " << text << "\n";
-   
-    for(int i = 0; i < text.length(); i++){
-        std::cout << text[i] << " " << (int)text[i] << "\n";
+    for(int i = 0; i < text.length(); i++){//loops through each index # of string
+        std::cout << text[i] << " " << (int)text[i] << "\n"; //type casts text[i] into int
+    }   //prints char and its ascii code
+    
+    /* a second test:
+    std::cout << "Input: " << text2 << "\n";
+    for(int i = 0; i < text2.length(); i++){
+        std::cout << text2[i] << " " << (int)text2[i] << "\n"; //type casts text[i] into int
     }
+    */
     return 0;
 }
