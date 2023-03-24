0195f1f98371744660a81fc8d0dc088b3bf65b7d
Daphne Luzong <daphneluzong@daphnes-air.lan>
2023-03-24 02:19:16 -0400

    finished lab

---
+++ funcs.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "imageio.h"
+
+void invert(std::string input);
+void invert_half(std::string input);
+void box(std::string input);
+void pixel(std::string input);
+void scale(std::string input);
+void pixelate(std::string input);
