023cd488ac72336d2b59733a78e532bf63520490
Daphne Luzong <daphneluzong@daphnes-air.lan>
2023-03-31 02:32:13 -0400

    completed lab

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,46 @@
 #include <iostream>
+#include <cmath>
 #include "funcs.h"
+#include "coord3d.h"
 
-// add functions here
+//Task A: Length and distance in 3D space
+double length(Coord3D *p){
+   double sq_x = pow(p->x, 2); //same as (*p).x to show class member
+   double sq_y = pow(p->y, 2); //squares the values
+   double sq_z = pow(p->z, 2);
+   double distance = sqrt(sq_x + sq_y + sq_z); //apply euclidian formula
+   return distance;
+}
 
+//Task B: Farther from the origin?
+Coord3D * fartherFromOrigin(Coord3D *p1, Coord3D *p2){
+    double len_p1 = length(p1);
+    double len_p2 = length(p2);
+    if(len_p1 > len_p2){
+        return p1;
+    }
+    else{
+        return p2;
+    }
+}
+
+//Task C: Velocity and moving objects
+void move(Coord3D *ppos, Coord3D *pvel, double dt){
+    ppos->x = ppos->x + pvel->x * dt;
+    ppos->y = ppos->y + pvel->y * dt;
+    ppos->z = ppos->z + pvel->z * dt;
+}
+
+//Task E: Creating and deleting objects dynamically
+// allocate memory and initialize
+Coord3D* createCoord3D(double x, double y, double z){
+    Coord3D *p = new Coord3D; //creates a new Coord3D object dynamically
+    p->x = x;
+    p->y = y;
+    p->z = z;
+    return p;
+}
+// free memory
+void deleteCoord3D(Coord3D *p){
+    delete p;
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,51 @@
+/*
+Daphne Luzong
+CSCI 13500
+Michael Zamansky
+Lab 9
+*/
+
 #include <iostream>
-
-
+#include <cmath>
+#include "coord3d.h"
 #include "funcs.h"
 
-int main()
-{
+int main(){
+  //Task A: Length and distance in 3D space
+  Coord3D pointP = {10, 20, 30};
+  std::cout << length(&pointP) << std::endl; //expected: 37.4166
+  
+  std::cout << "----------------------------------\n";
+  //Task B: Farthest from the origin?
+  //Utilizes pointP here too
+  Coord3D pointQ = {-20, 21, -22};
+
+  std::cout << "Address of P: " << &pointP << std::endl;
+  std::cout << "Address of Q: " << &pointQ << std::endl;
+
+  Coord3D * ans = fartherFromOrigin(&pointP, &pointQ);
+   
+  std::cout << "ans = " << ans << std::endl; //returns address of pointer that is further from the origin
+
+  std::cout << "----------------------------------\n";
+  //Task C:
+  Coord3D pos = {0, 0, 100.0};
+  Coord3D vel = {1, -5, 0.2};
+
+  move(&pos, &vel, 2.0); // object pos gets changed
+  std::cout << pos.x << " " << pos.y << " " << pos.z << std::endl;
+  // prints: 2 -10 100.4
+
+  std::cout << "----------------------------------\n";
+  //Task E:
+  Coord3D *ppos = createCoord3D(10,20,30); //position
+  Coord3D *pvel = createCoord3D(5.5,-1.4,7.77); //velocity
+  move(ppos, pvel, 10.0);
+  std::cout << "Coordinates after 10 seconds: " << ppos->x << " " << ppos->y << " " << ppos->z << std::endl;
+
+  //release memory
+  deleteCoord3D(ppos);
+  deleteCoord3D(pvel);
+
   return 0;
 }
